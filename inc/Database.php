feb5ebdecf184f310af4c5fd21bbec7eda5207cd
Vladislav Dudin <vladislav.dudin@ivkhk.ee>
2025-11-27 08:38:56 +0200

    second com

--- inc/Database.php
+++ inc/Database.php
@@ -17,5 +17,39 @@ class database {
     function __destruct() {
         $this->disconnect();
     }
+    function connect() {
+        if (!$this->conn) {
+            try {
+                $this->conn = new PDO('mysql:host='.$this->host.';dbname='.$this->
+                baseName.'', $this->user, $this->password, array(PDO::
+                MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'));
+            }
+            catch (Exception $e) {
+                die('Connection failed : ' . $e->getMessage());
+            }
+        }
+        return $this->conn;
+    }
+
+    function disconnect() {
+        if ($this->conn) {
+            $this->conn = null;
+        }
+    }
+
+    function getOne($query) {
+        $stmt = $this->conn->prepare($query);
+        $stmt->execute();
+        $stmt->setFetchMode(PDO::FETCH_ASSOC);
+        $response = $stmt->fetch();
+        return $response;
+    }
     
+    function getAll($query) {
+        $stmt = $this->conn->prepare($query);
+        $stmt->execute();
+        $stmt->setFetchMode(PDO:FETCH_ASSOC);
+        $response = $stmt->fetchAll();
+        return $response;
+    }
 }
