cd10d57f5f78e761bdcf77e673c0d3062c019c95
Vladislav Dudin <vladislav.dudin@ivkhk.ee>
2025-11-27 11:09:56 +0200

    work concept

--- view/layout.php
+++ view/layout.php
@@ -3,13 +3,11 @@
 <head>
     <title>NEWSPORTAL</title>
 
-    <link rel="stylesheet" 
-          href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
-          integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcW7x9JvoRxT2MZw1T"
-          crossorigin="anonymous">
+    <link rel="stylesheet"  
+      href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
 
-    <link rel="stylesheet" type="text/css" href="style.css">
-    <link href="https://fonts.googleapis.com/css?family=Noto+Serif" rel="stylesheet">
+        <link rel="stylesheet" type="text/css" href="style.css">
+        <link href="https://fonts.googleapis.com/css?family=Noto+Serif" rel="stylesheet">
     <meta charset="utf-8">
 </head>
 <body>
