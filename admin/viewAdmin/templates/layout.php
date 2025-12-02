81ed8e7f5eb472f60c2704e3447cf03df557e129
Vladislav Dudin <vladislav.dudin@ivkhk.ee>
2025-12-02 15:11:26 +0200

    Work Login

---
+++ admin/viewAdmin/templates/layout.php
@@ -0,0 +1,34 @@
+<html>
+<head>
+<title>Dashboard</title>
+<link href="public/css/bootstrap.css" rel="stylesheet">
+<link href="public/css/mystyle.css" rel="stylesheet">
+<!-- Font Awesome -->
+<link rel="stylesheet" href="public/css/font-awesome.min.css">
+<!-- SCRIPT -->
+<script src="public/js/jquery.min.js"></script>
+<script src="public/js/bootstrap.min.js"></script>
+<script type="text/javascript" src="public/js/ajaxupload.3.5.js"></script>
+</head>
+<body>
+    <div class="container">
+        <!--Я ЗАКОНЧИЛ ТУТ СМОТРИ СТРАНИЦУ 5---->
+        <div class="header clearfix">
+            <nav class="navbar navbar-default">
+                <div class="container-fluid">
+
+                </div>
+            </nav>
+        </div>
+
+        <div id="content" style="padding-top:20px;">
+
+        </div>
+
+        <footer class="footer">
+            <p>&copy; 2019 Design Admin dashboard <i class="fa fa-child"></i></p>
+        </footer>
+
+    </div> <!-- /container -->
+</body>
+</html>
